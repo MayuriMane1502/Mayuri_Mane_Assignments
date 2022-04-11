629db708f5e0b1a129c9550b61258596e4c90e18
MayuriMane1502 <67822253+MayuriMane1502@users.noreply.github.com>
2022-04-11 23:15:04 +0530

    Add files via upload

---
+++ Csharp Assignment-4_Exception/Exception_pgm.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Assignment_4_Exception
+{
+    class Stack
+    {
+        private int[] elet;
+        private int top;
+        private int max;
+        public Stack(int size)
+        {
+            elet = new int[size];
+            top = -1;
+            max = size;
+        }
+        public void push(int item)
+        {
+            if (top == max - 1)
+            {
+                throw new Exception("Stack Overflow can't perform push ");
+
+            }
+            else
+            {
+                elet[++top] = item;
+            }
+        }
+
+        public int pop()
+        {
+            if (top == -1)
+            {
+                throw new Exception("Stack is Empty");
+
+            }
+
+            else
+            {
+                Console.WriteLine("popped Element is: " + elet[top]);
+                return elet[top--];
+            }
+        }
+        public void printStack()
+        {
+            if (top == -1)
+            {
+                Console.WriteLine("Stack is Empty");
+                return;
+            }
+            else
+            {
+                for (int i = 0; i <= top; i++)
+                {
+                    Console.WriteLine("Item[" + (i + 1) + "]: " + elet[i]);
+                }
+            }
+        }
+    }
+    class Exception_pgm
+    {
+        static void Main()
+        {
+            Stack S = new Stack(5);
+            S.push(10);
+            S.push(20);
+            S.push(30);
+            S.push(40);
+            S.push(50);
+
+
+            Console.WriteLine("Items are: ");
+            S.printStack();
+            
+
+
+            S.pop();
+            S.pop();
+            S.pop();
+            S.pop();
+            S.pop();
+            Console.ReadKey();
+        }
+    }
+}
+
