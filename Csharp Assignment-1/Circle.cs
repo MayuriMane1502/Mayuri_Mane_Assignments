e3be38d9d52a4858907a90e15bd5a32a629dadc9
MayuriMane1502 <67822253+MayuriMane1502@users.noreply.github.com>
2022-04-11 23:03:14 +0530

    Add files via upload

---
+++ Csharp Assignment-1/Circle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Assignment_1_Circle
+{
+    internal class Circle
+    {
+        static void Main(string[] args)
+        {
+            Double pi = 3.14;
+            Console.WriteLine("enter the radius of circle :");
+            Double r = Convert.ToDouble(Console.ReadLine());
+            Double Area = pi * r * r;
+            Console.WriteLine("The Area of circle is {0}", Area);
+            
+            Double Circumference = 2 * pi * r;
+            Console.WriteLine("The Circumference of circle is {0}", Circumference);
+            Console.ReadKey();
+        }
+    }
+}
