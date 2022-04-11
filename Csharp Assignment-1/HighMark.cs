d1c1f64f1a91f50255c4a965fbe687cde9288769
MayuriMane1502 <67822253+MayuriMane1502@users.noreply.github.com>
2022-04-11 23:05:54 +0530

    Add files via upload

---
+++ Csharp Assignment-1/HighMark.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Assignment_1_HighMark
+{
+    internal class HighMark
+    {
+        static void Main(string[] args)
+        {
+            Double highestmark = 0;
+            int k = 0;
+            for (int i = 1; i <= 5; i++)
+            {
+
+                Console.WriteLine("Student {0}  please enter your average marks", i);
+                Double j = Convert.ToDouble(Console.ReadLine());
+                if (j > highestmark)
+                {
+                    highestmark = j;
+                    k = i;
+                }
+            }
+            Console.WriteLine("The average highest marks are {0} of student number {1}", highestmark, k);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Csharp Assignment-1/SwapValues.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Assignment_1_SwapValue
+{
+    internal class SwapValues
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n ...Before Swapping....");
+
+            Console.WriteLine("\n First Number is: {0}", num1);
+            Console.WriteLine(" Second Number is: {0}", num2);
+
+            num1 = num1 + num2;
+            num2 = num1 - num2;
+            num1 = num1 - num2;
+            Console.WriteLine("\n ...After Swapping....");
+
+            Console.WriteLine("\n First number is : {0}", num1);
+            Console.WriteLine("Second number is : {0}", num2);
+            Console.ReadKey();
+        }
+    }
+}
